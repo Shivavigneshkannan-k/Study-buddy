e6b3e4765db88ea0b2a3d27633129f439ec3bdc0
Shiva vignesh kannan.K <135090729+Shivavigneshkannan-k@users.noreply.github.com>
2024-10-15 00:53:57 +0530

    done

--- studybud/urls.py
+++ studybud/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("base.urls")),
     path("api/",include("base.api.urls")),
-    path("https://shivavigneshkannan-k.github.io/Study-buddy/",include("base.urls"))
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
